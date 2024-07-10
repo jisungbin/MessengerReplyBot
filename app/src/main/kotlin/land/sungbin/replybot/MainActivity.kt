3b8001c44df1dcad4cba804cd0a3c7baed75612f
Ji Sungbin <ji@sungb.in>
2024-07-10 22:36:28 +0900

    TODO components

--- app/src/main/kotlin/land/sungbin/replybot/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/replybot/MainActivity.kt
@@ -2,7 +2,7 @@
  * Developed by Ji Sungbin 2024.
  *
  * Licensed under the MIT.
- * Please see full license: https://github.com/jisungbin/MessengerReplyBot/blob/main/LICENSE
+ * Please see full license: https://github.com/jisungbin/MessengerReplyBot/blob/trunk/LICENSE
  */
 
 package land.sungbin.replybot
@@ -10,17 +10,26 @@ package land.sungbin.replybot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import land.sungbin.replybot.components.ReplierMainScreen
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
     setContent {
-      Text("Hello, World!", modifier = Modifier.fillMaxSize().background(Color.White))
+      Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = { TODO() },
+      ) { padding ->
+        ReplierMainScreen(modifier = Modifier.padding(padding))
+      }
     }
   }
 }
---
+++ app/src/main/kotlin/land/sungbin/replybot/components/ReplierMainScreen.kt
@@ -0,0 +1,17 @@
+/*
+ * Developed by Ji Sungbin 2024.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/jisungbin/MessengerReplyBot/blob/trunk/LICENSE
+ */
+
+package land.sungbin.replybot.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable fun ReplierMainScreen(
+  modifier: Modifier = Modifier,
+) {
+  TODO()
+}
